7c71f1679b495916db60d9fb4df95c304185f735
Sara <saramedernach@ucsb.edu>
2020-07-06 00:58:12 -0700

    olleh.cpp

---
+++ welcome/code/olleh.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+string reverse(string name);
+
+int main(int argc, char *argv[]) {
+
+    cout << reverse(argv[1]) << endl;
+
+    return 0;
+
+}
+
+string reverse(string name) {
+
+    string str = "Hello, " + name + "!";
+
+    string backwards = "";
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+
+        backwards += str[i];
+
+    }
+
+    return backwards;
+
+}
