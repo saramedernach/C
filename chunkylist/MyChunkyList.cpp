893741c1525e68b9ae4ef3ef30a2abd5d176bf53
Sara <saramedernach@ucsb.edu>
2020-07-13 16:59:39 -0700

    chunkylist

--- chunkylist/MyChunkyList.cpp
+++ chunkylist/MyChunkyList.cpp
@@ -37,6 +37,7 @@ void MyChunkyList::insert(int index, const string& item) {
         if (mCount == 0) {
 
             mHead = new MyChunkyNode(item, 0, mChunksize, nullptr, nullptr);
+            mTail = mHead;
             
 
         }
