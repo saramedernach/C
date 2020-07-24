19ee04c455cd0bffe6e9164000cdb17088ffbcaa
Sara <saramedernach@ucsb.edu>
2020-07-24 16:31:23 -0700

    convert

--- convert/main.cpp
+++ convert/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
   
-  if (argc != 3 || (((strcmp(argv[1], "prefix") != 0) || (strcmp(argv[1], "postfix") != 0)) && ((strcmp(argv[2], "prefix") != 0) || (strcmp(argv[2], "postfix") != 0) || (strcmp(argv[2], "infix") != 0)))) {
+  if (argc != 3) {
 
     cerr << "Usage: convert [input-format] [output-format]\n"
       << "  Valid input formats:   prefix, postfix\n"
@@ -17,7 +17,15 @@ int main(int argc, char** argv) {
     exit(0);
 
   }
-  /*else if (strcmp(argv[1], "prefix") != 0 || strcmp(argv[1], "prefix") != 0){
+  /*if (argc != 3 || (((strcmp(argv[1], "prefix") != 0) || (strcmp(argv[1], "postfix") != 0)) && ((strcmp(argv[2], "prefix") != 0) || (strcmp(argv[2], "postfix") != 0) || (strcmp(argv[2], "infix") != 0)))) {
+
+    cerr << "Usage: convert [input-format] [output-format]\n"
+      << "  Valid input formats:   prefix, postfix\n"
+      << "  Valid output formats:  prefix, infix, postfix\n";
+    exit(0);
+
+  }
+  else if (strcmp(argv[1], "prefix") != 0 || strcmp(argv[1], "prefix") != 0){
 
     cerr << "Usage: convert [input-format] [output-format]\n"
       << "  Valid input formats:   prefix, postfix\n"
