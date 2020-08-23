c96fa127fdc6148887f90150a59ca6f3630f259e
Sara <saramedernach@ucsb.edu>
2020-08-22 22:57:55 -0700

    vaccinate

--- vaccinate/MyScheduler.cpp
+++ vaccinate/MyScheduler.cpp
@@ -6,7 +6,117 @@ Scheduler* Scheduler::create(
   std::vector<std::string>            factories,
   std::vector<Route>                  routes
 ) {
-  return new MyScheduler(/* TODO */);
+  return new MyScheduler(MyScheduler(deadline, cities, factories, routes));
 }
 
 // MyScheduler Member Functions
+MyScheduler::MyScheduler(unsigned int deadline, map<string, unsigned int> cities, vector<string> factories, vector<Route> routes) {
+
+  mDeadline = deadline;
+  mRoutes = routes;
+
+  for (const auto& pair: cities) {
+
+    City city;
+    city.name       = pair.first;
+    city.factory    = false;
+    city.population = pair.second;
+    city.vaccines   = 0;
+
+    mCities[pair.first] = city;
+
+  }
+
+  for(const auto& name: factories) {
+
+    auto itr = mCities.find(name);
+
+    itr->second.factory = true;
+
+  }
+
+  for (auto route: mRoutes) {
+
+    auto itr = mCities.find(route.city1);
+    auto it = mCities.find(route.city2);
+
+    itr->second.routes.insert(route);
+    it->second.routes.insert(route);
+
+  }
+
+  for (const auto& city: mCities) {
+
+    if (city.second.factory) {
+
+      paths.push_back(shortestPath(city.second));
+
+    }
+
+  }
+
+}
+
+vector<Shipment> MyScheduler::schedule() {
+
+  vector<Shipment> shipments;
+  Shipment ship;
+  shipments.push_back(ship);
+  
+  /*for (auto& path: paths) {
+
+    
+
+  }*/
+
+  return shipments;
+
+}
+
+vector<pair<Route, int> > MyScheduler::shortestPath(City source) {
+
+  vector<pair<Route, int> > path;
+  priority_queue<pair<Route, int>, vector<pair<Route, int> >, CompareDays > pq;
+  int days = 0;
+
+  for (auto& route: source.routes) {
+
+    days = route.days;
+    pq.push(make_pair(route, route.days));
+
+  }
+
+  while (!pq.empty()) {
+
+    for (const auto& pair: path) {
+
+      if (pair.first.city2 == pq.top().first.city2) {
+
+        pq.pop();
+
+      }
+
+    }
+
+    path.push_back(make_pair(pq.top().first, days));
+
+    auto itr = mCities.find(pq.top().first.city2);
+    for (auto& route: itr->second.routes) {
+
+      days += route.days;
+      pq.push(make_pair(route, days));
+
+    }
+
+    pq.pop();
+
+  }
+
+  for (auto p: path) {
+
+    cout << p.first.city1 << " -> " << p.first.city2 << " " << p.second;
+
+  }
+  return path;
+
+}
--- vaccinate/MyScheduler.h
+++ vaccinate/MyScheduler.h
@@ -3,14 +3,54 @@
 
 #include "Scheduler.h"
 
-class MyScheduler: public Scheduler {
-  // Member Variables
+#include <queue>
+#include <list>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+class CompareDays {
+
 public:
+
+  bool operator () (pair<Route, int> &r1, pair<Route, int> &r2) {
+
+    return r1.second > r2.second;
+
+  }
+
+};
+
+class MyScheduler: public Scheduler {
+
+  struct City {
+
+    string   name;
+    bool          factory;
+    unsigned int  population;
+    unsigned int  vaccines;
+    set<Route>    routes;
+
+  };
+
+  // Member Variables
+  unsigned int mDeadline;
+  map<string, City> mCities;
+  vector<Route> mRoutes;
+  map<pair<string, string>, int> graph;
+  vector<vector<pair<Route, int> > > paths;
+
+public:
+
   // Constructor
-  // Destructor?
+  MyScheduler(unsigned int deadline, map<string, unsigned int> cities, vector<string> factories, vector<Route> routes);
 
   // Required Member Function
-  std::vector<Shipment> schedule();
+  vector<Shipment> schedule();
+
+  vector<pair<Route, int> > shortestPath(City source);
+
 };
 
 #endif
